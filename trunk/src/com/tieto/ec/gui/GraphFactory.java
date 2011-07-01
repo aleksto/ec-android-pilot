4f5251c371fa68a5fcf2b5296e5bbb2c576b24c9
alitorstensen@gmail.com <alitorstensen@gmail.com@e75f8645-b46e-2fa6-8d89-5d7775bfa960>
2011-07-01 07:33:44 +0000

    Added listeners to Checkboxes

---
+++ trunk/src/com/tieto/ec/gui/GraphFactory.java
@@ -0,0 +1,13 @@
+package com.tieto.ec.gui;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.widget.CheckBox;
+
+import com.androidplot.xy.LineAndPointFormatter;
+
+public class GraphFactory {
+	
+	//public GraphFactory(LineAndPointFormatter format, CheckBox checkBox, ArrayList<HashMap<String, String>> valueList)
+}
