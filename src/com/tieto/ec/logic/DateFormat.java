c1b255de5cf934b8d6b9bc97f0b7e5d615263a82
boge90@gmail.com <boge90@gmail.com@e75f8645-b46e-2fa6-8d89-5d7775bfa960>
2011-07-04 11:32:58 +0000

    Class for converting YYYY-MM-DDTHH:MM:SS string to long

---
+++ src/com/tieto/ec/logic/DateFormat.java
@@ -0,0 +1,20 @@
+package com.tieto.ec.logic;
+
+import java.util.Date;
+
+
+public class DateFormat {
+
+	public static long parse(String string){
+		//YYYY-MM-DDT00:00:00Z
+		int year = Integer.valueOf(string.substring(0, 4));
+		int month = Integer.valueOf(string.substring(5, 7));
+		int day = Integer.valueOf(string.substring(8, 10));
+		int hour = Integer.valueOf(string.substring(11, 13));
+		int min = Integer.valueOf(string.substring(14, 16));
+		int sec = Integer.valueOf(string.substring(17, 19));
+		
+		Date date = new Date(year, (month-1), day, hour, min, sec);
+		return date.getTime();
+	}
+}
