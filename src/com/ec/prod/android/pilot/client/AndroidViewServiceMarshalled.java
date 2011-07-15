fbb008a06c686c517c3f3d6cfea6968a977a7e20
alitorstensen@gmail.com <alitorstensen@gmail.com@e75f8645-b46e-2fa6-8d89-5d7775bfa960>
2011-07-15 13:17:21 +0000

    Adding new version of AndroidViewServiceMarshalled

--- src/com/ec/prod/android/pilot/client/AndroidViewServiceMarshalled.java
+++ src/com/ec/prod/android/pilot/client/AndroidViewServiceMarshalled.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import android.util.Log;
+
 import com.ec.prod.android.pilot.service.ViewServiceMarshalled;
-import com.ec.prod.android.pilot.client.WebserviceDateConverter;
-import com.ec.prod.android.pilot.client.Webservice;
+import com.tieto.ec.logic.DateFormat;
+import com.tieto.ec.webServices.Webservice;
 
 public class AndroidViewServiceMarshalled extends Webservice implements ViewServiceMarshalled{
 
@@ -14,72 +16,60 @@ public class AndroidViewServiceMarshalled extends Webservice implements ViewServ
 		super(username, password, namespace, url);
 	}
 
-	public List<String> getSections() {
+	public List<String> getSections() {	
 		Object response = executeWebservice("getSections", "");
-		String[] responseList = response.toString().split("return=");
-		List<String> list = new ArrayList<String>();
-
-		int idx;
-		for (int i = 1; i < responseList.length; i++) {
-			idx = responseList[i].lastIndexOf(";");
-			list.add(responseList[i].substring(0, idx));
-		}
-		return list;
+		return generateResponseList(response);
 	}
 
 	public List<String> getTableData(String section, Date fromdate, Date toDate, int resolution) {
-		Object response = executeWebservice("getTableData", "arg0", section,
-				"arg1", WebserviceDateConverter.parse(fromdate.getTime()),
-				"arg2", WebserviceDateConverter.parse(toDate.getTime()),
-				"arg3", Integer.toString(resolution));
-
-		String[] responseList = response.toString().split("return=");
-		List<String> list = new ArrayList<String>();
-
-		int idx;
-		for (int i = 1; i < responseList.length; i++) {
-			idx = responseList[i].lastIndexOf(";");
-			list.add(responseList[i].substring(0, idx));
-		}
-		return list;
+		Object response = executeWebservice("getTableData",  "arg0", section,
+														 	 "arg1", DateFormat.parse(fromdate.getTime()),
+															 "arg2", DateFormat.parse(toDate.getTime()),
+															 "arg3", Integer.toString(resolution));
+				return generateResponseList(response);
 	}
 
 	public List<String> getGraphDataBySection(String section, Date fromDate, Date toDate, int resolution) {
-		Object response = executeWebservice("getGraphDataBySection", "arg0", section,
-				"arg1", WebserviceDateConverter.parse(fromDate.getTime()),
-				"arg2", WebserviceDateConverter.parse(toDate.getTime()),
-				"arg3", Integer.toString(resolution));
-
-		String[] responseList = response.toString().split("return=");
-		List<String> list = new ArrayList<String>();
-
-		int idx;
-		for (int i = 1; i < responseList.length; i++) {
-			idx = responseList[i].lastIndexOf(";");
-			list.add(responseList[i].substring(0, idx));
-		}
-		return list;
+		Object response = executeWebservice("getGraphDataBySection",  "arg0", section,
+																 	  "arg1", DateFormat.parse(fromDate.getTime()),
+																	  "arg2", DateFormat.parse(toDate.getTime()),
+																	  "arg3", Integer.toString(resolution));
+		return generateResponseList(response);
 	}
 
-	public List<String> getGraphDataByRow(String row, Date fromDate, Date toDate, int resolution) {
-		//TODO
+	public List<String> getGraphDataByRow(String row, Date fromDate,
+			Date toDate, int resolution) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public List<String> getTextData(String section, Date fromDate, Date toDate,	int resolution) {
-		Object response = executeWebservice("getTextData", "arg0", section,
-				"arg1", WebserviceDateConverter.parse(fromDate.getTime()),
-				"arg2", WebserviceDateConverter.parse(toDate.getTime()),
-				"arg3", Integer.toString(resolution));
+	public List<String> getTextData(String section, Date fromDate, Date toDate, int resolution) {
+		Object response = executeWebservice("getTextData",  "arg0", section,
+															"arg1", DateFormat.parse(fromDate.getTime()),
+															"arg2", DateFormat.parse(toDate.getTime()),
+															"arg3", Integer.toString(resolution));
+		return generateResponseList(response);
+	}
 
-		String[] responseList = response.toString().split("return=");
+
+	private List<String> generateResponseList(Object response) {
 		List<String> list = new ArrayList<String>();
-
+		String responseString = response.toString();
+		String[] responseList = responseString.toString().split("return=");
+		
 		int idx;
 		for (int i = 1; i < responseList.length; i++) {
 			idx = responseList[i].lastIndexOf(";");
 			list.add(responseList[i].substring(0, idx));
-		}
+		}		
 		return list;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
