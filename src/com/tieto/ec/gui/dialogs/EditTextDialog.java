53d2c983bd8d40838efcb2d7131c75b8e6b1f47d
alitorstensen@gmail.com <alitorstensen@gmail.com@e75f8645-b46e-2fa6-8d89-5d7775bfa960>
2011-07-21 14:00:30 +0000

    Changed GUI to EditTextDialog

--- src/com/tieto/ec/gui/dialogs/EditTextDialog.java
+++ src/com/tieto/ec/gui/dialogs/EditTextDialog.java
@@ -9,10 +9,14 @@ import com.tieto.ec.logic.FileManager;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.view.Gravity;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class EditTextDialog extends Dialog{
 
@@ -24,19 +28,28 @@ public class EditTextDialog extends Dialog{
 		ScrollView scroll = new ScrollView(context);
 		TableLayout table = new TableLayout(context);
 		EditText editText = new EditText(context);
+		LinearLayout buttonLayout = new LinearLayout(context);
 		Button ok = new Button(context);
 		Button cancel = new Button(context);
-		
+		LinearLayout.LayoutParams param1 = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT);
+		LinearLayout.LayoutParams param2 = new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT, 1f);
+
 		//this
 		setTitle(title);
 		setContentView(scroll);
 		
+		//Button Layout
+		buttonLayout.setBackgroundResource(android.R.drawable.bottom_bar);
+		param2.gravity = Gravity.CENTER_VERTICAL;
+		
 		//Childs
 		scroll.addView(table);
 		table.addView(editText);
-		table.addView(ok);
-		table.addView(cancel);
+		table.addView(buttonLayout, param1);
+		buttonLayout.addView(ok, param2);
+		buttonLayout.addView(cancel, param2);
 		
+	
 		//Text
 		ok.setText("Ok");
 		cancel.setText("Cancel");
