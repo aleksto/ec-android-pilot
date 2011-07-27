6279adf23a0d2a359785646a7156a0aa9e85f6ca
alitorstensen@gmail.com <alitorstensen@gmail.com@e75f8645-b46e-2fa6-8d89-5d7775bfa960>
2011-07-27 08:12:29 +0000

    Commented on server classes, and also extended ExampleViewService

--- src/com/ec/prod/android/pilot/client/AndroidViewServiceMarshalled.java
+++ src/com/ec/prod/android/pilot/client/AndroidViewServiceMarshalled.java
@@ -3,20 +3,42 @@ package com.ec.prod.android.pilot.client;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 import com.ec.prod.android.pilot.service.ViewServiceMarshalled;
+import com.tieto.ec.activities.DailyMorningReport;
 import com.tieto.ec.logic.DateConverter;
 
 public class AndroidViewServiceMarshalled extends Webservice implements ViewServiceMarshalled{
 
+	/**
+	 * This class extends {@link Webservice} and handles the passing and receiving of data from an external webservice. 
+	 * It passes username, password, namespace and url to parent so that {@link Webservice} can find correct server and 
+	 * be authorized.
+	 * @param username
+	 * @param password
+	 * @param namespace
+	 * @param url
+	 */
 	public AndroidViewServiceMarshalled(String username, String password, String namespace, String url) {
 		super(username, password, namespace, url);
 	}
 
+	/**
+	 * This method gets the sections which is to be shown in {@link DailyMorningReport}. Inside a section there
+	 * can be text data, graph data or table data. 
+	 * The method passes the response to generateResonseList() which parses the response string into a set of strings
+	 * which it returns in a list
+	 */
 	public List<String> getSections() {	
 		Object response = executeWebservice("getSections", "");
 		return generateResponseList(response);
 	}
 
+	/**
+	 * This method gets the table data which is to be shown in {@link DailyMorningReport}
+	 * The method passes the response to generateResonseList() which parses the response string into a set of strings
+	 * which it returns in a list
+	 */
 	public List<String> getTableData(String section, Date fromdate, Date toDate, int resolution) {
 		Object response = executeWebservice("getTableData",  "arg0", section,
 														 	 "arg1", DateConverter.parse(fromdate, DateConverter.Type.DATE),
@@ -25,6 +47,11 @@ public class AndroidViewServiceMarshalled extends Webservice implements ViewServ
 				return generateResponseList(response);
 	}
 
+	/**
+	 * This method gets the graph data which is to be shown in {@link DailyMorningReport}
+	 * The method passes the response to generateResonseList() which parses the response string into a set of strings
+	 * which it returns in a list
+	 */
 	public List<String> getGraphDataBySection(String section, Date fromDate, Date toDate, int resolution) {
 		Object response = executeWebservice("getGraphDataBySection",  "arg0", section,
 																 	  "arg1", DateConverter.parse(fromDate, DateConverter.Type.DATE),
@@ -33,11 +60,7 @@ public class AndroidViewServiceMarshalled extends Webservice implements ViewServ
 		return generateResponseList(response);
 	}
 
-	public List<String> getGraphDataByRow(String row, Date fromDate,
-			Date toDate, int resolution) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 
 	public List<String> getTextData(String section, Date fromDate, Date toDate, int resolution) {
 		Object response = executeWebservice("getTextData",  "arg0", section,
@@ -47,7 +70,21 @@ public class AndroidViewServiceMarshalled extends Webservice implements ViewServ
 		return generateResponseList(response);
 	}
 
+	/**
+	 * Not yet implemented
+	 */
+	public List<String> getGraphDataByRow(String row, Date fromDate,
+			Date toDate, int resolution) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	/**
+	 * This method parses the response string which getSections(), getTableData(), getGraphDataBySection() and getTextData() passes it.
+	 * These methods can be found in this class. 
+	 * @param response
+	 * @return
+	 */
 	private List<String> generateResponseList(Object response) {
 		List<String> list = new ArrayList<String>();
 		String responseString = response.toString();
--- src/com/ec/prod/android/pilot/client/HttpTransportBasicAuth.java
+++ src/com/ec/prod/android/pilot/client/HttpTransportBasicAuth.java
@@ -24,7 +24,7 @@
  */
 package com.ec.prod.android.pilot.client;
 
-import java.io.*;
+import java.io.IOException;
 
 import org.ksoap2.transport.HttpTransportSE;
 import org.ksoap2.transport.ServiceConnectionSE;
