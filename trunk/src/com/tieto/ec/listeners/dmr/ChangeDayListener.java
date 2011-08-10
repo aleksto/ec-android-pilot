feb2d4e722667c3c620367ac83c3556c1dc8f1c7
alitorstensen@gmail.com <alitorstensen@gmail.com@e75f8645-b46e-2fa6-8d89-5d7775bfa960>
2011-08-10 08:54:24 +0000

    Fixed a custom DatePickerDialog

--- trunk/src/com/tieto/ec/listeners/dmr/ChangeDayListener.java
+++ trunk/src/com/tieto/ec/listeners/dmr/ChangeDayListener.java
@@ -1,6 +1,5 @@
 package com.tieto.ec.listeners.dmr;
 
-import java.util.Calendar;
 import java.util.Date;
 
 import android.app.DatePickerDialog;
@@ -11,6 +10,7 @@ import android.view.View.OnClickListener;
 import android.widget.DatePicker;
 
 import com.tieto.ec.activities.DailyMorningReport;
+import com.tieto.ec.logic.ChooseDate;
 import com.tieto.ec.logic.DateIntervalCalculator;
 
 public class ChangeDayListener implements OnClickListener, OnDateSetListener{
@@ -53,65 +53,15 @@ public class ChangeDayListener implements OnClickListener, OnDateSetListener{
 			break;
 		case CHOOSE_DAY:
 			date = dmr.getToDate();
-			Calendar c = Calendar.getInstance();
-			c.setTime(date);
-			
-			String dayOfWeek = getDayOfWeek(c.get(Calendar.DAY_OF_WEEK));
-			String monthOfYear = getMonthOfYear(c.get(Calendar.MONDAY));
-			
-			dialog = new DatePickerDialog(dmr, this, date.getYear()+1900, date.getMonth(), date.getDate());
-			dialog.setTitle(dayOfWeek + ", " + monthOfYear + " " + c.get(Calendar.DATE) + ", " + c.get(Calendar.YEAR));
+	
+			dialog = new ChooseDate(dmr, date, this, date.getYear()+1900, date.getMonth(), date.getDate());
+						
 			dialog.show();
 			break;
 		}
 	}
 
-	private String getMonthOfYear(int monthOfYear) {
-		if(monthOfYear == 0)
-			return "January";
-		if(monthOfYear == 1)
-			return "February";
-		if(monthOfYear == 2)
-			return "March";
-		if(monthOfYear == 3)
-			return "April";
-		if(monthOfYear == 4)
-			return "May";
-		if(monthOfYear == 5)
-			return "June";
-		if(monthOfYear == 6)
-			return "July";
-		if(monthOfYear == 7)
-			return "August";
-		if(monthOfYear == 8)
-			return "September";
-		if(monthOfYear == 9)
-			return "October";
-		if(monthOfYear == 10)
-			return "November";
-		if(monthOfYear == 11)
-			return "December";
-		return null;
-		
-	}
 
-	private String getDayOfWeek(int dayOfWeek) {
-		if(dayOfWeek == 1)
-			return "Sunday";
-		if(dayOfWeek == 2)
-			return "Monday";
-		if(dayOfWeek == 3)
-			return "Tuesday";
-		if(dayOfWeek == 4)
-			return "Wednsday";
-		if(dayOfWeek == 5)
-			return "Thursday";
-		if(dayOfWeek == 6)
-			return "Friday";
-		if(dayOfWeek == 7)
-			return "Saturday";		
-		return null;
-	}
 
 	/**
 	 * Sets new {@link Date} in the report when user clicks ok at {@link DatePickerDialog}
