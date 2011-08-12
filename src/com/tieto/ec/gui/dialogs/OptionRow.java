912f69a6e8a01d7729f1823d82783c074da704b8
boge90@gmail.com <boge90@gmail.com@e75f8645-b46e-2fa6-8d89-5d7775bfa960>
2011-08-12 10:59:53 +0000

    Default case goBack=false

--- src/com/tieto/ec/gui/dialogs/OptionRow.java
+++ src/com/tieto/ec/gui/dialogs/OptionRow.java
@@ -168,7 +168,7 @@ public class OptionRow extends RelativeLayout{
 		
 		case DEFAULT:
 			//Listener
-			goBack = true;
+			goBack = false;
 			setDefault = true;
 			setOnClickListener(new DialogActionListener(optionDialog, goBack, optionsTitle, nextState, setDefault));
 			break;
